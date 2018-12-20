efcc89d89f1e69d27e4c49e883daa580df77c856
Quy Truong <qmtruong2@gmail.com>
2018-12-19 16:32:45 -0800

    completed displaying list items with dummy data

---
+++ src/data/to_do_list.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        _id: '001',
+        title: 'wash Car',
+        details: 'wash the blue car',
+        complete: false
+    },
+    {
+        id: '002',
+        title: 'wash horse',
+        details: 'wash the brown horse',
+        complete: false
+    },
+    {
+        id: '003',
+        title: 'wash house',
+        details: 'wash the house',
+        complete: false
+    },
+    {
+        id: '004',
+        title: 'wash dog',
+        details: 'wash the dog',
+        complete: false
+
+    }
+];
